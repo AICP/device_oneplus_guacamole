eb7b3cd19189d9e8d3ca18ef12a6ec09fe6c5175
Ralf Luther <luther.ralf@gmail.com>
2020-01-09 20:15:32 +0100

    guacamole: move the camera around (1/2)
    Move the Camera Helper and Camera Motor service (from lineage) to the common tree.
    It will be build and used by motorized camera devices only anyway.
    Rename the the camera motor service from ".guacamole" to ".msmnile"
    
    Change-Id: I9a30ec1e8052c2a3a14197ab27b40f24e7ace15d

--- device.mk
+++ device.mk
@@ -15,7 +15,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 # Camera
 PRODUCT_PACKAGES += \
     vendor.lineage.camera.motor@1.0 \
-    vendor.lineage.camera.motor@1.0-service.guacamole \
+    vendor.lineage.camera.motor@1.0-service.msmnile \
     OnePlusCameraHelper
 
 # Device init scripts
